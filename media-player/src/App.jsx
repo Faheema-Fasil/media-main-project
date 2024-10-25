73bba47cfd2e89bda08125af9e626b3c6c57b6f6
Faheema_Fasil <faheemafasil12@gmail.com>
2024-10-25 12:28:47 +0530

    updates

--- media-player/src/App.jsx
+++ media-player/src/App.jsx
@@ -1,13 +1,23 @@
 
-
-
+import Header from './Components/Header'
+import {Routes,Route} from 'react-router-dom'
+import LandingPage from './Pages/LandingPage'
+import Home from './Pages/Home'
+import WatchHistory from './Pages/WatchHistory'
 import './App.css'
+import Footer from './Components/Footer'
 
 function App() {
 
   return (
     <>
-      
+      <Header/>
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/watch-history" element={<WatchHistory />} />
+      </Routes>
+      <Footer/>
     </>
   )
 }
--- media-player/src/Components/Header.jsx
+++ media-player/src/Components/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import {Container,Navbar} from 'react-bootstrap'
 
 function Header() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar style={{backgroundColor:'grey'}} >
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="https://cdn-icons-png.freepik.com/256/11620/11620813.png?semt=ais_hybrid"
+              width="50"
+              height="40"
+              className="d-inline-block align-top"
+            />{' '}
+            <span style={{
+              fontSize:'30px'
+            }}>MEDIA-PLAYER</span>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
