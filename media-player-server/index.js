071207ff95d987b982aba5d3fef71dbdb1638f84
Faheema_Fasil <faheemafasil12@gmail.com>
2024-10-29 21:39:43 +0530

    update

--- media-player-server/index.js
+++ media-player-server/index.js
@@ -3,7 +3,7 @@ const MpServer=JSONServer.create()
 const router=JSONServer.router('db.json')
 const middleware=JSONServer.defaults()
 
-const PORT= 3000 || process.env.PORT
+const PORT= 3001 || process.env.PORT
 
 MpServer.use(middleware)
 MpServer.use(router)
