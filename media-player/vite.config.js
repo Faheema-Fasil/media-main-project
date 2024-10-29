5145fae58b2a5af58c7bf1adb4d95508585e9092
Faheema_Fasil <faheemafasil12@gmail.com>
2024-10-29 21:54:09 +0530

    update

--- media-player/vite.config.js
+++ media-player/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/discount/',
+  base: '/your-repo-name/', // update this to match your repo name
 });
